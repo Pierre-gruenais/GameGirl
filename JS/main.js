6e3e02bbcf795879c7d8ed8d71f73fee0e765678
Pierre-gruenais <Pierre.gruenais@oclcock.school>
2024-06-10 17:37:26 +0200

    Iitial commit

--- JS/main.js
+++ JS/main.js
@@ -5,27 +5,26 @@ let score = 0;
 //fonction pour mettre a jour le score et afficher dans l'interface utilisateur
 function updateScore(points) {
   score += points;
-  document.getElementById('score-value').textContent = score;
-
+  document.getElementById("score-value").textContent = score;
 }
 updateScore(10);
 
 //fonction pour depllacer le personnage
-function moveCharacter(direction){
+function moveCharacter(direction) {
   // Déplacer le personnage horizontalement
-    // Mettre à jour la position du personnage en fonction de la direction
+  // Mettre à jour la position du personnage en fonction de la direction
 }
 
 //Fonction pour generer un objet qui tombe
-function generateFallingObject(){
-      // Créer un nouvel objet qui tombe et l'ajouter à la scène
-    // Animer l'objet en le faisant descendre verticalement
+function generateFallingObject() {
+  // Créer un nouvel objet qui tombe et l'ajouter à la scène
+  // Animer l'objet en le faisant descendre verticalement
 }
 
 //Fonction pour detecter les collisions entre le personnage et les objets
-function detectCollisions(){
-    // Vérifier si le personnage entre en collision avec un objet
-    // Si oui, augmenter le score et supprimer l'objet de la scène
+function detectCollisions() {
+  // Vérifier si le personnage entre en collision avec un objet
+  // Si oui, augmenter le score et supprimer l'objet de la scène
 }
 
 //utiliser des fonctions de minuterie
