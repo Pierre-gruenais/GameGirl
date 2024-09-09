acc50d756e0b65f6336a2fe2c1c84b2ed85a2a9a
Pierre-gruenais <Pierre.gruenais@oclcock.school>
2024-09-09 13:22:11 +0200

    Sauvegarde des modifications locales avant changement de branche

--- src/JS/game.js
+++ src/JS/game.js
@@ -1,68 +1,172 @@
-//const moveLeftSound = new Audio("/src/assets/sound/move.mp3");
-//const moveRightSound = new Audio("/src/assets/sound/move.mp3");
-
+// game.js
 document.addEventListener("DOMContentLoaded", () => {
-  const character = document.getElementById("character");
-  //const gameArea = document.getElementById("game-area");
   const gameCanvas = document.getElementById("game-canvas");
+  const startButton = document.getElementById("start-button");
+  const introOverlay = document.getElementById("intro-overlay");
+  const backgroundMusic = document.getElementById("background-music");
+  let soundEnabled = true;
+  let lives = 3;
+  let previousLives = lives;
+  let score = 0;
+  let gameStarted = false;
 
-  // //const objectFallSpeed = 2; // Vitesse de chute des objets en pixels par frame
-  //
-  // const startButton = document.getElementById("start-button");
-  // const introOverlay = document.getElementById("intro-overlay");
-  //
-  //
-  // let gameStarted = false; // Variable pour suivre l'état du jeu
-  //
-  // // Fonction pour démarrer le jeu
-  // function startGame() {
-  //   if (introOverlay) {
-  //     introOverlay.style.display = "none"; // Cache le bandeau d'accueil
-  //     console.log("bandeau caché !");
-  //     // Ajoutez ici l'initialisation ou le démarrage du jeu
-  //     // Par exemple, vous pouvez commencer à créer des objets qui tombent, etc.
-  //     console.log("Jeu démarré !");
-  //   } else {
-  //     console.error("l'element d'introOverlay est manquant.");
-  //   }
-  // }
-  //
-  // // Assurez-vous que les éléments existent avant de les utiliser
-  // if (startButton && introOverlay) {
-  //   startButton.addEventListener("click", startGame);
-  // } else {
-  //   console.error("Les éléments de l'intro n'ont pas été trouvés.");
-  // }
+  const images = [
+    "src/assets/images/bonbon.png",
+    "src/assets/images/bonboncoeur.png",
+    "src/assets/images/donut.png",
+    "src/assets/images/Glace.png",
+    "src/assets/images/sucetteCoeur.png",
+  ];
 
-  // Vérifie si les éléments existent
-  if (!character || !gameCanvas || !bottomLimit) {
-    // && !gameArea
-    console.error("Character, game area, or game canvas element not found");
-    return;
+  const malusImageUrl = [
+    "src/assets/images/carotte-2.png",
+    "src/assets/images/poivron.png",
+    "src/assets/images/epinards.png",
+    "src/assets/images/betterave.png",
+  ];
+
+  backgroundMusic.volume = 0.1;
+
+  function startGame() {
+    if (introOverlay) {
+      introOverlay.style.display = "none";
+      backgroundMusic
+        .play()
+        .catch((err) => console.error("Failed to play background music:", err));
+
+      setInterval(createFallingObject, 5000);
+      setInterval(createMalusObject, 10000);
+      updateLives();
+      updateScore(0);
+      gameStarted = true;
+    }
   }
 
-  // Fonction pour redimensionner le canvas
-  //function resizeCanvas() {
-  //  if (
-  //    gameCanvas instanceof HTMLCanvasElement // && gameArea instanceof HTMLElement
-  //  ) {
-  //    gameCanvas.width = window.innerWidth; //gameArea.clientWidth;
-  //    gameCanvas.height = window.innerHeight; //gameArea.clientHeight;
-  //  }
-  //}
+  function updateLives() {
+    const livesContainer = document.getElementById("lives-container");
+    if (livesContainer) {
+      livesContainer.innerHTML = "";
+      for (let i = 0; i < lives; i++) {
+        const lifeIcon = document.createElement("img");
+        lifeIcon.src = "src/assets/images/coeur(1).png";
+        lifeIcon.className = "life-icon";
+        livesContainer.appendChild(lifeIcon);
+      }
+      if (lives < previousLives) {
+        const lostLifeIcon = livesContainer.children[lives];
+        if (lostLifeIcon) {
+          lostLifeIcon.classList.add("lost-life");
+          setTimeout(() => {
+            lostLifeIcon.remove();
+          }, 500);
+        }
+      }
+      previousLives = lives;
+    }
+  }
 
-  // Redimensionne le canvas et initialise la position du personnage au chargement et lors du redimensionnement de la fenêtre
-  window.addEventListener("resize", () => {
-    // resizeCanvas();
-    initializeCharacterPosition();
-  });
+  function updateScore(points) {
+    score += points;
+    const scoreElement = document.getElementById("score-value");
+    if (scoreElement) {
+      scoreElement.textContent = score.toString();
+    }
+  }
 
-  // Initialisation au chargement
-  //resizeCanvas();
+  function detectCollisions(fallingObject) {
+    const character = document.getElementById("character");
+    const characterRect = character.getBoundingClientRect();
+    const objectRect = fallingObject.getBoundingClientRect();
 
-  // Démarrer l'animation des objets tombants
-  //setInterval(generateFallingObject, 1000); // Génère un nouvel objet toutes les secondes
-  // animateFallingObjects();
+    if (
+      characterRect.left < objectRect.right &&
+      characterRect.right > objectRect.left &&
+      characterRect.top < objectRect.bottom &&
+      characterRect.bottom > objectRect.top
+    ) {
+      if (fallingObject.className === "falling-object") {
+        updateScore(1);
+      } else if (fallingObject.className === "malus-object") {
+        lives -= 1;
+        updateLives();
+        if (lives <= 0) {
+          alert("Game over");
+        }
+      }
+      fallingObject.remove();
+    }
+  }
+
+  function getFallSpeed() {
+    if (score >= 50) {
+      return Math.random() * 6 + Math.pow(score, 1.05)/ 10; // vitesse exponentielle a partir de 60 points
+       } else if (score >= 40) {
+      return Math.random() * 5.5 + 2.5; // vitesse plus rapide a partir de  40 points
+    } else if (score >= 30) {
+      return Math.random() * 5 + 2; // vitesse plus rapide a partir de 30 points
+    } else if (score >= 20) {
+      return Math.random() * 4 + 2; // vitesse plus rapide a partir de  20 points
+    } else if (score >= 10) {
+      return Math.random() * 3 + 1.5; // vitesse plus rapide a 10 points
+    } else {
+      return Math.random() * 2 + 1; //vitesse de base
+    }
+  }
+
+  function createFallingObject() {
+    if (!gameStarted) return;
+    const fallingObject = document.createElement("div");
+    fallingObject.className = "falling-object";
+    const randomImage = images[Math.floor(Math.random() * images.length)];
+    fallingObject.style.backgroundImage = `url(${randomImage})`;
+    fallingObject.style.left = `${
+      Math.random() * (gameCanvas.clientWidth - 50)
+    }px`;
+    fallingObject.style.top = "0px";
+    document.body.appendChild(fallingObject);
+
+    let top = 0;
+    const fallSpeed = getFallSpeed();
+    const fallInterval = setInterval(() => {
+      top += fallSpeed;
+      fallingObject.style.top = `${top}px`;
+      detectCollisions(fallingObject);
+
+      if (top > window.innerHeight - fallingObject.clientHeight) {
+        clearInterval(fallInterval);
+        fallingObject.remove();
+      }
+    }, 1000 / 60);
+  }
+
+  function createMalusObject() {
+    if (!gameStarted) return;
+    const malusObject = document.createElement("div");
+    malusObject.className = "malus-object";
+    const randomMalusImage =
+      malusImageUrl[Math.floor(Math.random() * malusImageUrl.length)];
+    malusObject.style.backgroundImage = `url(${randomMalusImage})`;
+    malusObject.style.left = `${
+      Math.random() * (gameCanvas.clientWidth - 50)
+    }px`;
+    malusObject.style.top = "0px";
+    document.body.appendChild(malusObject);
+
+    let top = 0;
+    const fallSpeed = getFallSpeed();
+    const fallInterval = setInterval(() => {
+      top += fallSpeed;
+      malusObject.style.top = `${top}px`;
+      detectCollisions(malusObject);
+
+      if (top > window.innerHeight - malusObject.clientHeight) {
+        clearInterval(fallInterval);
+        malusObject.remove();
+      }
+    }, 1000 / 60);
+  }
+
+  if (startButton && introOverlay) {
+    startButton.addEventListener("click", startGame);
+  }
 });
-
-console.log("Script Javascript chargé avec succès !");
